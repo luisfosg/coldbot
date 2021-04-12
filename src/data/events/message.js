be0d18608dd5dfca799e9852c80d7081d503fac8
Luis Osorio <luisfernandogarciaosorio@gmail.com>
2021-04-12 15:31:25 -0500

    comandos mencionando al bot

--- src/data/events/message.js
+++ src/data/events/message.js
@@ -2,19 +2,23 @@ import { sendMsg } from '../util';
 import { getPrefix } from '../../db/prefix';
 import { checkArgs } from '../functions/checkargs';
 
-export default async ( client, msg ) => {
-	if ( msg.author.bot ) return;
-
-	const PREFIX = await getPrefix( msg );
-	client.prefix = PREFIX;
-
+const mentionBot = async ( client, msg ) => {
 	if ( msg.content.startsWith( `<@!${client.user.id}>` ) ) {
-		sendMsg( msg, `Hola, El Prefix es: \`${ PREFIX }\`` );
-		return;
-	}
+		if ( msg.content === `<@!${client.user.id}>` ) {
+			sendMsg( msg, `Hola, El Prefix es: \`${ client.prefix }\`` );
+			return;
+		}
 
-	if ( msg.content.startsWith( PREFIX ) ) {
-		const args = msg.content.slice( PREFIX.length ).trim().split( / +/ );
+		const data = `<@!${client.user.id}>`;
+		const args = msg.content.slice( data.length ).trim().split( / +/ );
+		args.unshift( client.prefix );
+		msg.content = args.join( ' ' );
+	}
+};
+
+const mentionPrefix = async ( client, msg ) => {
+	if ( msg.content.startsWith( client.prefix ) ) {
+		const args = msg.content.slice( client.prefix.length ).trim().split( / +/ );
 		let CMD = args.shift().toLowerCase();
 
 		if ( client.commands.find( ( c ) => c.alias.includes( CMD ) ) ) {
@@ -40,4 +44,14 @@ export default async ( client, msg ) => {
 			msg.reply( `Faltan Argumentos, Descripción: ${ commandFind.description }` );
 		}
 	}
+};
+
+export default async ( client, msg ) => {
+	if ( msg.author.bot ) return;
+
+	const PREFIX = await getPrefix( msg );
+	client.prefix = PREFIX;
+
+	mentionBot( client, msg );
+	mentionPrefix( client, msg );
 };
