c2a7c75f4711913b8b69b51ed77cc62c29e0be69
Luis Osorio <luisfernandogarciaosorio@gmail.com>
2021-04-12 16:30:03 -0500

    corrigiendo bug de profile.

--- src/data/commands/profile.js
+++ src/data/commands/profile.js
@@ -7,8 +7,20 @@ export default {
 	alias: ['prf'],
 	args: 0, /* Indica el menor numero de argumentos posibles */
 	description: 'Da la Informacion de nosotros o si mencionamos a alguien nos da la Informacion de esta persona.',
-	execute: async ( _client, msg, _args ) => {
-		const dataUser = msg.mentions.members.first() || msg.member;
+	execute: async ( _client, msg, args ) => {
+		let user;
+		let error = false;
+
+		if ( args[0] ) {
+			const id = args[0].replace( /[<]|!|@|[>]/g, '' );
+			user = await msg.guild.members.fetch( id ).catch( () => {
+				sendMsg( msg, 'El Usuario No Existe' );
+				error = true;
+			} );
+			if ( error ) return;
+		}
+
+		const dataUser = user || msg.member;
 		const embed = new MessageEmbed();
 
 		embed.setColor( 'RANDOM' );
