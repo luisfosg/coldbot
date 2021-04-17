b62419531e941c90bae15897c45c16348597b4d8
Luis Osorio <luisfernandogarciaosorio@gmail.com>
2021-04-16 21:53:21 -0500

    configurando comando help

--- src/data/commands/help.js
+++ src/data/commands/help.js
@@ -1,4 +1,4 @@
-import { sendMsg } from '../util';
+import { sendMsg, getConfig } from '../util';
 
 export default {
 	name: 'help',
@@ -10,7 +10,12 @@ export default {
 		permissions: [],
 	},
 	run: async ( client, msg, _args ) => {
-		msg.reply( `Hola, El prefix es \`${ client.prefix }\`` );
+		const config = await getConfig();
+
+		if ( !config.splitStrings[0] ) return msg.reply( `Hola, El prefix es \`${ client.prefix }\`` );
+
+		msg.reply( `Hola, El prefix es \`${ client.prefix }\`, y el separador de argumentos es \`${ config.splitStrings[1] }\`` );
+
 		sendMsg( msg, ' = = = Lista de Comandos = = = ' );
 		client.commands.map( ( c ) => sendMsg( msg, `> ${ client.prefix } ${ c.name }` ) );
 	},
