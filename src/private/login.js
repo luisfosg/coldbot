19cf91581ec9ff92a3091ebde30c2656ad3651d4
Luis Osorio <luisfernandogarciaosorio@gmail.com>
2021-05-06 20:21:47 -0500

    obteniendo los webhook por si mismos.

--- src/private/login.js
+++ src/private/login.js
@@ -1,17 +1,19 @@
+/* eslint-disable no-console */
 import { WebhookClient } from 'discord.js';
 
 export const password = process.env.PASSWORD;
-export const webhookIdWelcome = process.env.WEBHOOKIDWELCOME;
-export const webhookTokenWelcome = process.env.WEBHOOKTOKENWELCOME;
-export const webhookIdLogs = process.env.WEBHOOKIDLOGS;
-export const webhookTokenLogs = process.env.WEBHOOKTOKENLOGS;
+const webhookLogs = process.env.WEBHOOKLOGS;
+const webhookWelcome = process.env.WEBHOOKWELCOME;
+
+const welcomeHook = webhookWelcome.split( '/' ).slice( 5, 7 );
+const logsHook = webhookLogs.split( '/' ).slice( 5, 7 );
 
 export const connectWelcome = () => {
-	const hook = new WebhookClient( webhookIdWelcome, webhookTokenWelcome );
+	const hook = new WebhookClient( welcomeHook[0], welcomeHook[1] );
 	return hook;
 };
 
 export const connectLogs = () => {
-	const hook = new WebhookClient( webhookIdLogs, webhookTokenLogs );
+	const hook = new WebhookClient( logsHook[0], logsHook[1] );
 	return hook;
 };
