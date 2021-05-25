80925be08498ca04a13ab55bf809e3fbf3a41b8a
Luis Osorio <luisfernandogarciaosorio@gmail.com>
2021-05-24 19:02:03 -0500

    agregando descripciones a los comandos (EN).

--- src/data/commands/bot/server.js
+++ src/data/commands/bot/server.js
@@ -2,6 +2,8 @@ import { MessageEmbed } from 'discord.js';
 
 import { color } from '../../util';
 
+import language from '../../functions/language';
+
 export default {
 	name: 'serverinfo',
 	alias: ['server'],
@@ -16,17 +18,18 @@ export default {
 		visible: true,
 		permissions: [],
 	},
-	run: async ( _client, msg, _args ) => {
+	run: async ( client, msg, _args ) => {
+		const lang = language( client, msg.guild );
 		const server = msg.guild;
 
 		const embed = new MessageEmbed();
 		embed.setThumbnail( server.iconURL() );
 		embed.setAuthor( server.name, server.iconURL );
-		embed.addField( 'ID', server.id, true );
-		embed.addField( 'Region', server.region, true );
-		embed.addField( 'Creado el', server.joinedAt.toLocaleDateString(), true );
-		embed.addField( 'Dueño del Servidor', `${ server.owner.user.username }#${ server.owner.user.discriminator }`, true );
-		embed.addField( 'Miembros', server.memberCount, true );
+		embed.addField( lang.server.id, server.id, true );
+		embed.addField( lang.server.region, server.region, true );
+		embed.addField( lang.server.date, server.joinedAt.toLocaleDateString(), true );
+		embed.addField( lang.server.owner, `${ server.owner.user.username }#${ server.owner.user.discriminator }`, true );
+		embed.addField( lang.server.members, server.memberCount, true );
 		embed.setColor( color() );
 
 		msg.channel.send( embed );
