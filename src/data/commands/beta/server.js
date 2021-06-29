67a9e3309b7d394a951b9b0dd89d202874f32ed2
Luis Osorio <luisfosgdev@gmail.com>
2021-06-28 20:46:32 -0500

    agregando mas informacion en "serverinfo"

--- src/data/commands/beta/server.js
+++ src/data/commands/beta/server.js
@@ -1,6 +1,37 @@
+/* eslint-disable max-len */
 import { sendEmbed, getDate } from '../../util';
 
 import language from '../../functions/language';
+
+const filterLevels = {
+	DISABLED: 'Off',
+	MEMBERS_WITHOUT_ROLES: 'No Role',
+	ALL_MEMBERS: 'Everyone'
+};
+
+const verificationLevels = {
+	NONE: 'None',
+	LOW: 'Low',
+	MEDIUM: 'Medium',
+	HIGH: '(╯°□°）╯︵ ┻━┻',
+	VERY_HIGH: '┻━┻ ﾐヽ(ಠ益ಠ)ノ彡┻━┻'
+};
+
+const regions = {
+	brazil: 'Brazil',
+	europe: 'Europe',
+	hongkong: 'Hong Kong',
+	india: 'India',
+	japan: 'Japan',
+	russia: 'Russia',
+	singapore: 'Singapore',
+	southafrica: 'South Africa',
+	sydeny: 'Sydeny',
+	'us-central': 'US Central',
+	'us-east': 'US East',
+	'us-west': 'US West',
+	'us-south': 'US South'
+};
 
 export default {
 	name: 'serverinfo',
@@ -20,15 +51,42 @@ export default {
 	run: async ( _client, msg, _args ) => {
 		msg.channel.startTyping();
 
-		const lang = language( { guild: msg.guild } );
-
+		const roles = msg.guild.roles.cache.sort( ( a, b ) => b.position - a.position ).map( ( role ) => role.toString() );
+		const members = msg.guild.members.cache;
+		const channels = msg.guild.channels.cache;
+		const emojis = msg.guild.emojis.cache;
 		const server = msg.guild;
 
+		const lang = language( { guild: msg.guild } );
+
 		const fields = [
-			[lang.server.region, server.region, true],
-			[lang.server.date, getDate( { lang, date: server.createdTimestamp } )],
+			[lang.server.region, regions[server.region], true],
 			[lang.server.owner, `<@${server.owner.user.id}>`, true],
-			[lang.server.members, server.memberCount, true]
+			[lang.server.date, getDate( { lang, date: server.createdTimestamp } )],
+			['General', `
+			> **Name:** ${server.name}
+			> **Boost Tier:** ${server.premiumTier ? `Tier ${server.premiumTier}` : 'None'}
+			> **Explicit Filter:** ${filterLevels[server.explicitContentFilter]}
+			> **Verification Level:** ${verificationLevels[server.verificationLevel]}
+			`, true],
+			['Presence', `
+			> **Online:** ${members.filter( ( member ) => member.presence.status === 'online' ).size}
+			> **Idle:** ${members.filter( ( member ) => member.presence.status === 'idle' ).size}
+			> **Do Not Disturb:** ${members.filter( ( member ) => member.presence.status === 'dnd' ).size}
+			> **Offline:** ${members.filter( ( member ) => member.presence.status === 'offline' ).size}
+			`, true],
+			['Statistics', `
+			> **Emoji Count:** ${emojis.size}
+			> **Regular Emoji Count:** ${emojis.filter( ( emoji ) => !emoji.animated ).size}
+			> **Animated Emoji Count:** ${emojis.filter( ( emoji ) => emoji.animated ).size}
+			> **Member Count:** ${server.memberCount}
+			> **Humans:** ${members.filter( ( member ) => !member.user.bot ).size}
+			> **Bots:** ${members.filter( ( member ) => member.user.bot ).size}
+			> **Text Channels:** ${channels.filter( ( channel ) => channel.type === 'text' ).size}
+			> **Voice Channels:** ${channels.filter( ( channel ) => channel.type === 'voice' ).size}
+			> **Boost Count:** ${server.premiumSubscriptionCount || '0'}
+			`],
+			[`Roles [${roles.length - 1}]`, roles.join( ', ' )]
 		];
 
 		sendEmbed( {
